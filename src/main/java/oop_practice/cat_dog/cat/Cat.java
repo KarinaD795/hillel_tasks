56355192cf5b270b38e18403c785390773c474d1
KarinaD795 <karinagrva@gmail.com>
2023-08-08 14:13:50 +0200

    homework 15

--- src/main/java/oop_practice/cat_dog/cat/Cat.java
+++ src/main/java/oop_practice/cat_dog/cat/Cat.java
@@ -1,4 +1,19 @@
 package oop_practice.cat_dog.cat;
 
-public class Cat {
+public class Cat extends oop_practice.cat_dog.animal.Animal {
+    private String name;
+
+    public Cat(String name) {
+        super();
+
+        this.name = name;
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("I'am a Cat. My name is " + this.name + ". Mew");
+    }
 }
+
+
+
--- src/main/java/oop_practice/cat_dog/dog/Dog.java
+++ src/main/java/oop_practice/cat_dog/dog/Dog.java
@@ -1,5 +1,16 @@
 package oop_practice.cat_dog.dog;
 
-public class Dog {
+public class Dog extends oop_practice.cat_dog.animal.Animal {
+    private String name;
 
+    public Dog(String name) {
+        super();
+
+        this.name = name;
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("I'am a Dog. My name is " + this.name + ". Gav");
+    }
 }
